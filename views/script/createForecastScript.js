dd465e4e708f3be3d42a1c0b375f3963698dc6fe
Mishamba <misha.nenahov@gmail.com>
2022-06-03 17:22:34 +0300

    Create Forecast page + starting working on scripts

---
+++ views/script/createForecastScript.js
@@ -0,0 +1,27 @@
+function sendForecast()
+{
+    let forecastDataElement = document.getElementById("forecast_data")
+
+    let assetName = forecastDataElement.getElementById("asset_name").value
+    let targetDate = forecastDataElement.getElementById("target_date").value
+    let methodType = forecastDataElement.getElementById("method_type").value
+
+    TODO
+}
+
+function getMethodDateByMethodType(methodType, methodDataElement) {
+
+}
+
+function getMethodDataForTwoMarksForecast(methodDataElement) {
+
+}
+
+function getMethodDataForThreeMarksForecast(methodDataElement) {
+
+}
+
+function getMethodDataForThreeMarksWithChancesForecast(methodDataElement) {
+
+}
+
